d91014dd488d7218a627d0d416539d39e847a13c
arpitchavan <arpitchvn@gmail.com>
2023-09-04 10:24:31 +0530

    trial-1

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,59 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
+    public String generateAccountNumber(int digits, int sum) throws Exception {
+        if (digits <= 0 || sum < 0 || sum > 9 * digits) {
+            throw new Exception("Account Number cannot be generated");
+        }
 
-        return null;
+        StringBuilder accountNumber = new StringBuilder();
+        int remainingSum = sum;
+
+        for (int i = 0; i < digits - 1; i++) {
+            int digit = Math.min(remainingSum, 9);
+            accountNumber.append(digit);
+            remainingSum -= digit;
+        }
+
+        if (remainingSum > 9) {
+            throw new Exception("Account Number cannot be generated");
+        }
+
+        accountNumber.append(remainingSum);
+        return accountNumber.toString();
     }
 
     public void deposit(double amount) {
-        //add amount to balance
-
+        if (amount > 0) {
+            balance += amount;
+        }
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+        if (balance - amount < minBalance) {
+            throw new Exception("Insufficient Balance");
+        }
 
+        if (amount > 0) {
+            balance -= amount;
+        }
     }
 
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
 }
